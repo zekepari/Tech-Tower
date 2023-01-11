c05859d9e0953b7341f03e61c61ac0d261371d53
Zeke Parry <pariezekiel@gmail.com>
2023-01-12 00:02:23 +1000

    e

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Navbar, Container } from 'react-bootstrap'
+
+function Navbar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container fluid>
+
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Navbar;
